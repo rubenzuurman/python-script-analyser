10a73dd086231f95fbd16d1cb1111056a7a51f3b
unknown <rubenzuurman@gmail.com>
2023-07-07 00:20:50 +0200

    Implement get filename from commandline arguments

---
+++ doc/readme.md
@@ -0,0 +1,8 @@
+# Readme
+
+Goal of the project:
+    - Analyse a python script.
+    - Notify the user when an import is not used.
+    - Notify the user when a function is never called.
+    - Notify the user when a refenced variable is either not defined or 'out of scope'. This doesn't give an error but sometimes leads to undesired behaviour. This is the primary function of the program.
+    - Notify the user of inconsistent indentation.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Get command line arguments.
+    let args: Vec<String> = env::args().collect();
+    
+    // Make sure there is at least one commandline argument.
+    if args.len() <= 1 {
+        println!("Usage: python-script-analyser.exe <filename>");
+        return;
+    }
+    
+    // Assume that the first argument is the filename of the python script.
+    let filename: &str = &args[1];
+    
+    println!("{}", filename);
 }
